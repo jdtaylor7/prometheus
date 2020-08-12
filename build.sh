35c51f4abd007b23b0b840f4b87bcef91b7b319e
Jordan Taylor <jdtaylor7@gmail.com>
2020-08-11 23:39:14 -0700

    Upgrade build script.

--- build.sh
+++ build.sh
@@ -1,20 +1,34 @@
-# # !/bin/sh
-#
-# # Exit if any command fails.
-# set -e
-#
-# rm -rf ./build
-# mkdir build && cd build
-#
-# cmake -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-# cmake --build .
-
 #!/bin/sh
 
 # Exit if any command fails.
 set -e
 
+while getopts "rt:h" flag
+do
+    case $flag in
+    r) r_flag=1 ;;
+    t) t_flag=1
+       t_val="$OPTARG" ;;
+    h | ?) echo "Usage: $0: [-r][-t target]"
+           echo "   -r: Rebuild build directory"
+           echo "   -t: Specify build target"
+           exit 2;;
+    esac
+done
+
+if [ ! -z "$r_flag" ]; then
+    echo "Rebuilding build directory"
+    rm -rf ./build
+fi
+mkdir build 2>/dev/null
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-cmake --build . --target main
+
+if [ ! -z "$t_flag" ]; then
+    echo "Building target $t_val"
+    cmake --build . --target "$t_val"
+else
+    echo "Building all targets"
+    cmake --build .
+fi
